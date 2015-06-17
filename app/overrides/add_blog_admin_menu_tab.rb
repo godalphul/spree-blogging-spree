345a860db95536786aa8ca47bb4148390a027277
Luke Patrick <luke@snodandjeff.com>
2015-06-17 15:28:28 -0500

    Updates sidebar to work with Spree 3.0

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
 	:virtual_path => "spree/layouts/admin",
 	:name => "blog_admin_tabs",
 	insert_bottom: '#main-sidebar',
-	:text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') if can? :manage, Spree::BlogEntry %>",
-	:disabled => false
+	:partial => 'spree/admin/add_blog_bar'
 	)
